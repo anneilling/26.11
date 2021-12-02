5e34c17cf3580705a53d88dc4b7245773a110c18
opilane <opilane@HP-CZC6297BBL>
2021-12-02 09:50:40 +0200

    444

--- _26.11.py
+++ _26.11.py
@@ -1,12 +1,26 @@
-from module1 import*
+
+from module1 import *
+from keyboard import *
+palgad=loe_failist_listisse("palgad.txt")
+inimesed=loe_failist_listisse("inimesed.txt")
 while 1:
-    print("A - Sisesta,\n E - Ekraanile,\n")
-    valik=input()
-    if valik.lower()=="A":
-       inimesed,palgad=sisesta_andmed(inimesed,palgad)
-    elif valik.lower()=="E":
-        andmed_ekranile(inimesed,palgad)
-    elif valik.lower()=="K":
-        inimesed,palgad=kustuta(inimesed,palgad)
-    
+    print("*"*100,"\nKeskmine - 1\nMaksimaalne -2\nOtsing -3\nLisa - 4")
+    print("Vali ")
+
+    if read_key()=="1":
+        kesk=keskmine(inimesed,palgad)
+        print("Keskmine palk on ",summa)
+
+    elif read_key()=="2":
+        suurim,kellel=suurim(inimesed,palgad)
+        print("Maksimaalne palk=> ", suurim, " Kellel=> ",kellel)
+   
+    elif read_key()=="3":
+        vastus=otsing_nimi_jargi(inimesed, palgad)
+        print(vastus)
+
+    elif read_key()=="4":
+        palgad,inimesed=lisa(palgad,inimesed)
+        print(palgad,inimesed)
+
     
--- module1.py
+++ module1.py
@@ -1,33 +1,42 @@
-palgad=[1200,2500,750,395,1200]
-inimesed=["A","B","C","D","E"]
-def otsing_nimi_jargi(inimesed:list,palgad:list):
-    """Tagastame järjend või tekst
-    :rtype var: tulemus
+  #funktsioonide loomine
+from random import *
+def loe_failist_listisse(file:str)->list:
+    """Loeme tekst failist ja salvesta järjendisse
     """
+    file=open(file,"r")
+    list_=[]
+    for stroka in file:
+        list_.append(stroka.strip())
+    file.close()
+    return list_
+
+def otsing_nimi_jargi(inimesed:list,palk:list): #otsime inimest nimi järgi
     nimi=input("Keda otsime?")
     for inimene in inimesed:
-        if inimene==nimi:
+        if inimene.upper()==nimi.upper():
             n=inimesed.count(nimi)
-            print("Inimene on olemas, selline nimi kohtutakse ",n,"korda")
+            print("Leitud",n, "korda")
             b=0
             t=[]
             for i in range(n):
                 k=inimesed.index(nimi,b)
-                palk=palgad[k]
+                palk=palk[k]
                 b+=k+1
                 t.append(nimi+str(palk))
-            else:
-                t=print("Nimekirjas puudub")
-            return t
+                print(nimi, palk)
+        else:
+            t="Ei ole nimi kirjas"
+    return t
 
-def lisamine(i, p): #i-inimeste list, p-palgade list
-    kogus=int(input("Lisa kasutajad"))
-    for j in range(kogus):
-        nimi=input("Name: ")
-        i.append(nimi)
-        palk=int(input("Palk: "))
-        p.append(palk)
-        return i,p
+
+def lisa(palk:list,inimesed:list): #i-inimeste list, p-palgade list
+    """ Lisab inimesi ja nende palga
+    """
+    a=input("Sissesta nimi ")
+    inimesed.append(a)
+    b=int(input("Sisesta palk "))
+    palk.append(b)
+    return palk,inimesed
 
 def kutsutamine(i,p):
     try:
@@ -42,15 +51,19 @@ def kutsutamine(i,p):
             i.pop(a)
             return i,p
 
-def suurim_palk(i:list,p.list):
+def suurim(i:list,p:list):
+
     #otsine suurim palk ja näitame kellel ta on
     #:rtype float,str;
     suurim=max(p)
-    b=p,index(suurim)
+    b=p.index(suurim)
     kellel=i[b]
     return suurim,kellel
 
-def keskmine(i,p):
+def keskmine(i:list,p:list):
+    """Keskmise palka leidmine. Kui ta on loetelus, siis näiame kes saab seda kätte
+    :rtype var:
+    """
     summa=0
     for palk in p:
         summa+=palk
@@ -63,8 +76,5 @@ def keskmine(i,p):
     else:
         kesk="Puudub"
         return kesk
-
-
-
 
 
